1f17ff2f176adafc598f4499d6f8d4b15498f41a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-01 18:00:50 +1000

    removed functools

--- src/toaster/database/database.py
+++ src/toaster/database/database.py
@@ -7,7 +7,6 @@ About:
     File describing the Databse class.
 """
 
-from functools import singledispatch
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import Session, sessionmaker, declarative_base, DeclarativeBase
 from loguru import logger
@@ -23,22 +22,19 @@ class Database:
         self._engine = create_engine(connection_uri, echo=debug, pool_pre_ping=True)
         self._session = sessionmaker(bind=self._engine, autoflush=False)
 
-    @singledispatch
     def create_tables(self) -> None:
         """Creates all tables defined in the SQLAlchemy BaseModel."""
 
         logger.info("Creating tables...")
         BaseModel.metadata.create_all(self._engine)
 
-    @singledispatch
     def drop_tables(self) -> None:
         """Drops all tables defined in the SQLAlchemy BaseModel."""
 
         logger.info("Droping tables...")
         BaseModel.metadata.create_all(self._engine)
 
-    @create_tables.register
-    def _(self, base: DeclarativeBase):
+    def create_tables_site(self, base: DeclarativeBase):
         """Creates all tables defined in the provided SQLAlchemy base model.
 
         Args:
@@ -46,8 +42,7 @@ class Database:
         """
         base.metadata.create_all(self._engine)
 
-    @drop_tables.register
-    def _(self, base: DeclarativeBase):
+    def drop_tables_site(self, base: DeclarativeBase):
         """Drops all tables defined in the provided SQLAlchemy base model.
 
         Args:
