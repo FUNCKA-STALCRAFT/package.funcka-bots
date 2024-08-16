2675c0f2d3bc231c9ffdc97f35e6995cb255b484
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-16 20:49:54 +1000

    Refactor, removed legacy methods.

--- src/toaster/database/database.py
+++ src/toaster/database/database.py
@@ -8,8 +8,12 @@ About:
 """
 
 from sqlalchemy import create_engine, Engine
-from sqlalchemy.orm import Session, sessionmaker, declarative_base, DeclarativeBase
-from loguru import logger
+from sqlalchemy.orm import (
+    Session,
+    sessionmaker,
+    declarative_base,
+    DeclarativeBase,
+)
 
 
 BaseModel = declarative_base()
@@ -19,36 +23,48 @@ class Database:
     """SQLA Database class."""
 
     def __init__(self, connection_uri: str, debug: bool = False) -> None:
-        self._engine = create_engine(connection_uri, echo=debug, pool_pre_ping=True)
-        self._session = sessionmaker(bind=self._engine, autoflush=False)
+        self._engine = create_engine(
+            connection_uri,
+            echo=debug,
+            pool_pre_ping=True,
+        )
+        self._session = sessionmaker(
+            bind=self._engine,
+            autoflush=False,
+        )
 
-    def create_tables(self) -> None:
-        """Creates all tables defined in the SQLAlchemy BaseModel."""
-
-        logger.info("Creating tables...")
-        BaseModel.metadata.create_all(self._engine)
-
-    def drop_tables(self) -> None:
-        """Drops all tables defined in the SQLAlchemy BaseModel."""
-
-        logger.info("Droping tables...")
-        BaseModel.metadata.create_all(self._engine)
-
-    def create_tables_site(self, base: DeclarativeBase):
-        """Creates all tables defined in the provided SQLAlchemy base model.
+    def create_tables(self, base: DeclarativeBase):
+        """Creates all tables defined in the provided
+        SQLAlchemy base model.
 
         Args:
-            base (Any): The base model containing metadata for tables to be created.
+            base (Any): The base model containing
+            metadata for tables to be created.
         """
-        base.metadata.create_all(self._engine)
 
-    def drop_tables_site(self, base: DeclarativeBase):
-        """Drops all tables defined in the provided SQLAlchemy base model.
+        metadata = base.metadata
+        metadata.create_all(self._engine)
+
+    def drop_tables(self, base: DeclarativeBase):
+        """Drops all tables defined in the provided
+        SQLAlchemy base model.
 
         Args:
-            base (Any): The base model containing metadata for tables to be dropped.
+            base (Any): The base model containing
+            metadata for tables to be dropped.
         """
-        base.metadata.drop_all(self._engine)
+
+        metadata = base.metadata
+        metadata.drop_all(self._engine)
+
+    def make_session(self) -> Session:
+        """Creates and returns a new SQLAlchemy session.
+
+        Returns:
+            Session: A new SQLAlchemy session.
+        """
+
+        return self._session()
 
     @property
     def engine(self) -> Engine:
@@ -57,12 +73,5 @@ class Database:
         Returns:
             Engine: The SQLAlchemy engine instance.
         """
+
         return self._engine
-
-    def make_session(self) -> Session:
-        """Creates and returns a new SQLAlchemy session.
-
-        Returns:
-            Session: A new SQLAlchemy session.
-        """
-        return self._session()
