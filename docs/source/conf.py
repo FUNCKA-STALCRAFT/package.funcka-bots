5c7e35cfbabee28309909dad808bfba11b408a72
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-21 17:21:18 +1000

    Sphinx documentation initial commit (#2)

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "funcka-bots"
+version = "1.7"
+copyright = "2024, Oidaho (Ruslan Baskinsky)"
+author = "Oidaho (Ruslan Baskinsky)"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
