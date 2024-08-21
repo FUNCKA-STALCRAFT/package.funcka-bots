57640ee6261e0d3651189af25800b213751fb349
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-21 21:06:09 +1000

    A simple abstract handler. Created with a margin for expansion (#5).

---
+++ src/funcka_bots/handler/abc.py
@@ -0,0 +1,11 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+
+class ABCHandler(ABC):
+    """Abstract handler class."""
+
+    @abstractmethod
+    def __call__(self, event: Any) -> None:
+        """It is necessary to implement the logic of the handler's work."""
+        pass
