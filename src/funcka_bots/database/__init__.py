36892c58dd34e7df8228e722599963c0f2fb4ede
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-21 20:18:31 +1000

    New object and functions added to __init__.py (#1)

--- src/funcka_bots/database/__init__.py
+++ src/funcka_bots/database/__init__.py
@@ -7,14 +7,17 @@ About:
     Initializing the "database" module.
 """
 
-from .connection import build_connection_uri
-from .database import Database, BaseModel
-from .scripts import script
+from .connection import build_connection_uri, build_sqlite_uri
+from .database import Database, BaseModel, AsyncDatabase
+from .scripts import script, async_script
 
 
 __all__ = (
     "build_connection_uri",  # A function to build SQLAlchemy connection URI.
+    "build_sqlite_uri",
     "script",  # A decorator to mark functions as custom scripts for SQLAlchemy.
+    "async_script",
     "Database",  # Class encapsulating SQLAlchemy database management operations.
+    "AsyncDatabase",
     "BaseModel",  # Base class for SQLAlchemy models.
 )
