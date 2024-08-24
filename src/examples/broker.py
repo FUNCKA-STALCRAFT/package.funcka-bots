5de81f940c90bb1367d82c8fef3a4e110c24a130
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-24 15:51:16 +1000

    Examples of using the broker shell (#3)

---
+++ src/examples/broker.py
@@ -0,0 +1,30 @@
+from funcka_bots.broker.events import BaseEvent
+from funcka_bots.broker import (
+    Subscriber,
+    Publisher,
+    build_connection,
+)
+from .credentials import RedisCredentials
+
+
+publisher = Publisher(client=build_connection(creds=RedisCredentials))
+subscriber = Subscriber(client=build_connection(creds=RedisCredentials))
+
+
+def listen() -> None:
+    for event in subscriber.listen(channel_name="my_channel"):
+        print(f"Получено новое событие: {event}")
+
+        # Some actions with event
+
+
+def public(event: BaseEvent) -> None:
+    publisher.publish(obj=event, channel_name="other_channel")
+
+
+# The broker shell implemented over Redis is able to
+# accept objects, serialize and send them to
+# one of the Redis databases. Therefore, I'm importing the library
+# # funka_bots in 2 different services, they will send each other
+# # other objects that can be used in the future
+# code. Their functionality and all their attributes are fully preserved.
