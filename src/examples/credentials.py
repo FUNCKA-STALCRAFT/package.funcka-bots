5e887aac3ff69b63b671a1fcc1dfea8c92f977b6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-24 15:16:02 +1000

    Comments on the examples (#3)

--- src/examples/credentials.py
+++ src/examples/credentials.py
@@ -5,6 +5,17 @@ from funcka_bots.credentials import (
 )
 
 
+# # These data structures are based on NamedTuple
+# in fact, they are DTO,
+# with which you can quickly and conveniently
+# transfer the connection\session data to the class
+# databases\message broker\cache.
+
+# # Perfect for defining them inside
+# files like "config.py".
+
+
+# Data for connecting to MySQL DBMS
 alchemy_creds = AlchemyCredentials(
     host="mysql.database.io",
     port=2206,
@@ -12,12 +23,14 @@ alchemy_creds = AlchemyCredentials(
     pswd="cool_bot_secret_pswd",
 )
 
+# Data for generating the database URL in SQLAlchemy
 alchemy_setup = AlchemySetup(
     dialect="mysql",
     driver="pymysql",
     database="test_schema",
 )
 
+# Connection data to Redis
 redis_creds = RedisCredentials(
     host="my.redis.io",
     port=2263,
