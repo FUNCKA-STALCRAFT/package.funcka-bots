746321c8214e2cfb83bb753c0a26056fe17ddbf9
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-25 19:18:13 +1000

    Log text changed. (#6)

--- src/funcka_bots/broker/base.py
+++ src/funcka_bots/broker/base.py
@@ -20,10 +20,10 @@ class BaseWorker:
 
         except Exception:
             channel_tag = f"Channel #{channel.channel_number}: "
-            logger.warning(channel_tag + f"No queue named {queue_name} found.")
+            logger.warning(channel_tag + f"No queue named '{queue_name}' found.")
 
             channel.queue_declare(queue=queue_name)
-            logger.info(channel_tag + f"Creating a new queue {queue_name}.")
+            logger.info(channel_tag + f"Creating a new queue '{queue_name}'.")
 
     @staticmethod
     def _serialize(obj: Any) -> ByteString:
